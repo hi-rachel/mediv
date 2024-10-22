9f92a1557a859b47ab03181d11ffc17ec54e2816
hi-rachel <rachel.uiux@gmail.com>
2024-10-22 14:36:08 +0900

    style: 조직도 이미지 변경, 제목 추가, 애니메이션 추가

--- app/components/about/Organization.tsx
+++ app/components/about/Organization.tsx
@@ -1,18 +1,31 @@
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const Organization = () => {
   return (
-    <div className="text-center">
-      <h2 className="text-3xl font-semibold text-center text-primary mb-8">
-        조직도
-      </h2>
-      <Image
-        src="/organization.png"
-        alt="Mediv 조직도"
-        width={600}
-        height={600}
-        className="mx-auto contain"
-      />
+    <div>
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className="text-center relative py-12"
+      >
+        <h2 className="text-5xl font-extrabold text-primary">ORGANIZATION</h2>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+      >
+        <div className="relative w-full h-96 text-center py-10">
+          <Image
+            src="/organization.png"
+            alt="Mediv 조직도"
+            fill
+            className="object-contain h-96"
+          />
+        </div>
+      </motion.div>
     </div>
   );
 };
