ae21c100b5afa50bcdd5ef0c27c7dfc0d4bae015
hi-rachel <rachel.uiux@gmail.com>
2024-10-30 10:21:08 +0900

    fix: @typescript-eslint/no-unused-vars 에러 고침

--- app/[locale]/terms/components/ContactSection.tsx
+++ app/[locale]/terms/components/ContactSection.tsx
@@ -1,9 +1,6 @@
-import { useTranslations } from "next-intl";
 import { ContactSectionProps } from "../types";
 
 const ContactSection = ({ emailTitle, contacts }: ContactSectionProps) => {
-  const t = useTranslations("PrivacyPolicy");
-
   return (
     <div className="bg-gray-50 p-6 rounded-lg">
       <div className="space-y-4">
