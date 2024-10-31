e4a71dc86434ab0bac3c97da6cb5f4801b4f25a5
hi-rachel <rachel.uiux@gmail.com>
2024-10-31 10:14:52 +0900

    chore(organization): 조직도 업데이트

--- app/[locale]/about/Organization.tsx
+++ app/[locale]/about/Organization.tsx
@@ -8,7 +8,7 @@ const Organization = () => {
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
-        className="text-center relative py-12"
+        className="text-center py-12"
       >
         <h2 className="text-4xl md:text-5xl font-bold text-primary">
           ORGANIZATION
@@ -19,12 +19,12 @@ const Organization = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, delay: 0.2 }}
       >
-        <div className="relative w-full h-96 text-center py-10">
+        <div className="w-full sm:h-[500px] h-[300px] flex justify-center text-center py-5">
           <Image
             src="/organization.png"
             alt="Mediv 조직도"
             fill
-            className="object-contain h-96"
+            className="md:pl-28 pl-4 object-contain"
           />
         </div>
       </motion.div>
