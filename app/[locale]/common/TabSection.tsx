71021a79a81f31e418127ee49a95a2cdc2776629
hi-rachel <rachel.uiux@gmail.com>
2024-10-31 16:44:12 +0900

    style(TabSection.tsx): 탭 글자 반응형 설정

--- app/[locale]/common/TabSection.tsx
+++ app/[locale]/common/TabSection.tsx
@@ -47,7 +47,7 @@ const TabSection = ({ menuId }: { menuId: string }) => {
             <div
               key={tab.id}
               onClick={() => handleTabClick(tab.id)}
-              className={`cursor-pointer px-6 py-4 text-lg font-semibold text-center border-b-4 transition-colors duration-300 ${
+              className={`cursor-pointer py-4 md:text-lg text-sm px-4 md:px-6 font-semibold text-center border-b-4 transition-colors duration-300 ${
                 activeTab === tab.id
                   ? "border-info text-info bg-gray-50"
                   : "border-transparent text-gray-600 hover:text-info"
