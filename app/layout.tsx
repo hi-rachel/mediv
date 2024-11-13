e4fd7ad2496e41cc69abde6504e4b6077d59cddb
hi-rachel <rachel.uiux@gmail.com>
2024-11-13 13:18:14 +0900

    feat: 네이버 웹마스터도구 등록

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html>
+      <Head>
+        <meta
+          name="naver-site-verification"
+          content="9fde815546ec9ba5b642f0b2f746fc765f1cb0b4"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
