328c414d4f400bb7690babc1a9b5e62178706260
hi-rachel <rachel.uiux@gmail.com>
2024-11-21 17:31:05 +0900

    chore: clarity Script 추가
    - env 업데이트

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { useLocale } from "next-intl";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,11 +14,24 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <Head>
         <meta
           name="naver-site-verification"
-          content="9fde815546ec9ba5b642f0b2f746fc765f1cb0b4"
+          content={process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION}
         />
       </Head>
-      <body className={inter.className}>{children}</body>
-      <GoogleAnalytics gaId="G-LZ6Y7SHGXL" />
+      <body className={inter.className}>
+        {children}
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+        </Script>
+      </body>
+      <GoogleAnalytics
+        gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ""}
+      />
     </html>
   );
 };
